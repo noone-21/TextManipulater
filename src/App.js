c2b74d45d4ac315d7fbea251ecee61f58dd41fdd
Danish <83973204+noone-21@users.noreply.github.com>
2022-07-26 12:32:57 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,64 @@
+// import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import About from './components/About';
+import React, {useState} from 'react'
+import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+function App() {
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+
+ 
+  const showAlert=(msg,type)=>{
+      setAlert({
+        msg: msg,
+        type: type
+      })
+      setTimeout(() => {
+        setAlert(null);
+      }, 1000);
+  }
+
+  const toggleMode=() => { 
+    if(mode==='light'){
+      setMode('dark')
+      document.body.style.backgroundColor="#2C3333"
+      document.body.style.color="#E7F6F2"
+      showAlert("Dark Mode has been enabled","success")
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor="#E7F6F2"
+      document.body.style.color="#2C3333"
+      showAlert("Light Mode has been enabled","success")
+    }
+  }
+  return (
+    <>
+    <Router>
+      <Navbar title="Navbar"  mode={mode} toggleMode={toggleMode}  />
+      <div className="container my-3">
+      <Switch>
+            <Route path="/about">
+              <About/>
+            </Route>
+            <Route path="/">
+              <TextForm showAlert={showAlert} heading ="ENTER YOUR TEXT:" mode={mode}/>
+            </Route>
+      </Switch>      
+      <Alert alert= {alert} />
+      </div>
+    </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/TextForm.js
@@ -0,0 +1,109 @@
+import React, {useState} from 'react'
+
+export default function TextForm(props) {
+    const [text, setText] = useState("");   
+
+    const handleUpClick=()=>{
+        let newText=text.toUpperCase();
+        setText(newText);
+        if(text!==""){
+            props.showAlert("Converted to UPPER CASE","success")
+        }else{
+            props.showAlert("Enter text to perform an action!","warning")
+        }
+    }
+    const handleLowClick=()=>{
+        let newText=text.toLowerCase();
+        setText(newText);
+        if(text!==""){
+            props.showAlert("Converted to lower case","success")
+        }else{
+            props.showAlert("Enter text to perform an action!","warning")
+        }    
+    }
+    const handleCapCaseClick=()=>{
+        const arr=text.split(" ");
+        for(let i=0; i<arr.length;i++){
+            arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+        }
+        setText(arr.join(" "));
+        if(text!==""){
+            props.showAlert("Converted to Capitalized Case","success")
+        }else{
+            props.showAlert("Enter text to perform an action!","warning")
+        }
+        
+    }
+    const handleSCaseClick=()=>{
+        const arr=text.split(".");
+        for(let i=0; i<arr.length;i++){
+            if(arr[i].charAt(0)===" "){
+                arr[i]=arr[i].slice(1);
+                arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+                arr[i]=" " + arr[i];
+            }else{
+                arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+            }
+        }
+        setText(arr.join("."));
+        if(text!==""){
+            props.showAlert("Converted to Sentence case","success")
+        }else{
+            props.showAlert("Enter text to perform an action!","warning")
+        }    
+    }
+    const handleAltCaseClick=()=>{
+        let newText = text.toLowerCase().split("");
+        for (let i = 0; i < newText.length; i += 2) {
+        newText[i] = newText[i].toUpperCase();
+        }
+        setText(newText.join(""));
+        if(text!==""){
+            props.showAlert("Converted to AlTeRnAtE CaSe","success")
+        }else{
+            props.showAlert("Enter text to perform an action!","warning")
+        } 
+    }
+    const handleClrClick=()=>{
+        let newText="";
+        setText(newText);
+        if(text!==""){
+            props.showAlert("Text cleared","success")
+        }else{
+            props.showAlert("Enter text to Clear!","warning")
+        }    
+    }
+    const handleExtraSpaces=()=>{
+        let newText=text.split(/[ ]+/);
+        setText(newText.join(" "))
+    }
+    const handleOnChange=(event)=>{
+        setText(event.target.value)
+    }  
+  return (
+<div  >
+<div  >
+    <h1 style={{color:props.mode==='dark'?'#A5C9CA':'black'}}>{props.heading}</h1>
+    <div className="form-group">
+    <textarea style={{backgroundColor:props.mode==='dark'?'#395B64':'white',borderColor:props.mode=== 'dark'?'#2C3333':'#A5C9CA',
+    borderRadius:'30px'}} 
+    className="form-control" value={text} id="myBox" rows="8" onChange={handleOnChange}></textarea>
+    </div>
+    <button  style={{color:props.mode==='dark'?'#A5C9CA':'black'}} className={`btn ${props.mode==="dark"?'btn-dark':'btn-light'} my-3 mx-1`} onClick={handleUpClick}>UPPER CASE</button>
+    <button  style={{color:props.mode==='dark'?'#A5C9CA':'black'}} className={`btn ${props.mode==="dark"?'btn-dark':'btn-light'} my-3 mx-1`} onClick={handleLowClick}>lower case</button>
+    <button  style={{color:props.mode==='dark'?'#A5C9CA':'black'}} className={`btn ${props.mode==="dark"?'btn-dark':'btn-light'} my-3 mx-1`} onClick={handleCapCaseClick}>Capitalized Case</button>
+    <button  style={{color:props.mode==='dark'?'#A5C9CA':'black'}} className={`btn ${props.mode==="dark"?'btn-dark':'btn-light'} my-3 mx-1`} onClick={handleSCaseClick}>Sentence case</button>
+    <button  style={{color:props.mode==='dark'?'#A5C9CA':'black'}} className={`btn ${props.mode==="dark"?'btn-dark':'btn-light'} my-3 mx-1`} onClick={handleAltCaseClick}>ALtErNaTiNg CaSe</button>
+    <button  style={{color:props.mode==='dark'?'#A5C9CA':'black'}} className={`btn ${props.mode==="dark"?'btn-dark':'btn-light'} my-3 mx-1`} onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+    <button  style={{color:props.mode==='dark'?'#A5C9CA':'black'}} className={`btn ${props.mode==="dark"?'btn-dark':'btn-light'} my-3 mx-1`} onClick={handleClrClick}>Clear Text</button>
+</div>
+<footer className="container my-3">
+    <h1 style={{color:props.mode==='dark'?'#A5C9CA':'black'}}>TEXT SUMMARY</h1>
+    <p style={{color:props.mode==='dark'?'#A5C9CA':'black'}}>{text.split(" ").filter(function(n) { return n !== ''&& n !=='\n' }).length} word(s) and {text.split("").filter(function(n) { return n !== ' '&& n !== '\n' }).length}{" "}  
+    character(s) and {text.split("\n").filter(function(n) { return n !== '' }).length} line(s)</p>
+</footer>
+</div>
+
+    
+  )
+}
